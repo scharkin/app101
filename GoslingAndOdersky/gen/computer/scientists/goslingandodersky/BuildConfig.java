1b3b4a32dd8a88413fff906bc6e42aeda2d78cc2
Sergey Charkin <sergey.charkin@gmail.com>
2015-03-28 16:37:26 -0400

    cleanup

--- GoslingAndOdersky/gen/computer/scientists/goslingandodersky/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package computer.scientists.goslingandodersky;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Nightsong/gen/translate/goethe/poem/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package translate.goethe.poem;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
